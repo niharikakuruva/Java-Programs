0e4f95b640f2680f4924f64b8bd8f82057022fd9
niharikakuruva <niharika798179@gmail.com>
2026-08-12 18:55:49 +0530

    Add comment for area of circle formula
    Added a comment explaining the area formula for clarity.

---
+++ CircleArea.java
@@ -0,0 +1,8 @@
+public class CircleArea {
+    public static void main(String[] args) {
+        double radius = 5;
+      //Formula: Area of Circle = π × radius × radius
+        double area = 3.14 * radius * radius;
+        System.out.println("Area of Circle = " + area);
+    }
+}
