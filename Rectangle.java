9a5a6193e2894c915225898753542dfd71d5d113
niharikakuruva <niharika798179@gmail.com>
2026-08-12 18:57:58 +0530

    Fix formatting issues in Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,12 @@
+public class Rectangle {
+    public static void main(String[] args) {
+        double length = 10;
+        double width = 5;
+        // Formula: Area = length × width
+        double area = length * width;
+        // Formula: Perimeter = 2 × (length + width)
+        double perimeter = 2 * (length + width);
+        System.out.println("Area of Rectangle = " + area);
+        System.out.println("Perimeter of Rectangle = " + perimeter);
+    }
+}
