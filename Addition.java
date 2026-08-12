3837ac54ec5c1e3e7afd0e707c6ada914b74dc48
niharikakuruva <niharika798179@gmail.com>
2026-08-12 18:35:29 +0530

    Add Addition class to calculate sum of two numbers

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        int firstNumber = 20;
+        int secondNumber = 30;
+        int sum = firstNumber + secondNumber;
+        System.out.println("Sum = " + sum);
+    }
+}
