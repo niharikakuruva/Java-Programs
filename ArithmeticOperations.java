44f725f48f628753ea066e69a9ecfe9bb20f092e
niharikakuruva <niharika798179@gmail.com>
2026-08-12 18:40:47 +0530

    Create ArithmeticOperations.java

---
+++ ArithmeticOperations.java
@@ -0,0 +1,18 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int firstNumber = 20;
+        int secondNumber = 5;
+
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        int quotient = firstNumber / secondNumber;
+        int remainder = firstNumber % secondNumber;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + difference);
+        System.out.println("Product = " + product);
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+    }
+}
